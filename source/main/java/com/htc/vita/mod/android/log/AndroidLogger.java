001bbf43d5405a1de852696ce3731e491b7288d8
Kene Lin <kenelin@gmail.com>
2020-07-16 13:25:01 +0800

    Minor update

--- source/main/java/com/htc/vita/mod/android/log/AndroidLogger.java
+++ source/main/java/com/htc/vita/mod/android/log/AndroidLogger.java
@@ -1,5 +1,6 @@
 package com.htc.vita.mod.android.log;
 
+import android.util.Log;
 import com.htc.vita.core.log.Logger;
 
 public class AndroidLogger extends Logger {
@@ -9,57 +10,57 @@ public class AndroidLogger extends Logger {
 
     @Override
     protected void onDebug(String tag, String message) {
-        android.util.Log.d(tag, message);
+        Log.d(tag, message);
     }
 
     @Override
     protected void onDebug(String tag, String message, Exception exception) {
         if (exception == null) {
-            android.util.Log.d(tag, message);
+            Log.d(tag, message);
         } else {
-            android.util.Log.d(tag, message, exception);
+            Log.d(tag, message, exception);
         }
     }
 
     @Override
     protected void onError(String tag, String message) {
-        android.util.Log.e(tag, message);
+        Log.e(tag, message);
     }
 
     @Override
     protected void onError(String tag, String message, Exception exception) {
         if (exception == null) {
-            android.util.Log.e(tag, message);
+            Log.e(tag, message);
         } else {
-            android.util.Log.e(tag, message, exception);
+            Log.e(tag, message, exception);
         }
     }
 
     @Override
     protected void onFatal(String tag, String message) {
-        android.util.Log.e(tag, message);
+        Log.e(tag, message);
     }
 
     @Override
     protected void onFatal(String tag, String message, Exception exception) {
         if (exception == null) {
-            android.util.Log.wtf(tag, message);
+            Log.wtf(tag, message);
         } else {
-            android.util.Log.wtf(tag, message, exception);
+            Log.wtf(tag, message, exception);
         }
     }
 
     @Override
     protected void onInfo(String tag, String message) {
-        android.util.Log.i(tag, message);
+        Log.i(tag, message);
     }
 
     @Override
     protected void onInfo(String tag, String message, Exception exception) {
         if (exception == null) {
-            android.util.Log.i(tag, message);
+            Log.i(tag, message);
         } else {
-            android.util.Log.i(tag, message, exception);
+            Log.i(tag, message, exception);
         }
     }
 
@@ -70,29 +71,29 @@ public class AndroidLogger extends Logger {
 
     @Override
     protected void onTrace(String tag, String message) {
-        android.util.Log.v(tag, message);
+        Log.v(tag, message);
     }
 
     @Override
     protected void onTrace(String tag, String message, Exception exception) {
         if (exception == null) {
-            android.util.Log.v(tag, message);
+            Log.v(tag, message);
         } else {
-            android.util.Log.v(tag, message, exception);
+            Log.v(tag, message, exception);
         }
     }
 
     @Override
     protected void onWarn(String tag, String message) {
-        android.util.Log.w(tag, message);
+        Log.w(tag, message);
     }
 
     @Override
     protected void onWarn(String tag, String message, Exception exception) {
         if (exception == null) {
-            android.util.Log.w(tag, message);
+            Log.w(tag, message);
         } else {
-            android.util.Log.w(tag, message, exception);
+            Log.w(tag, message, exception);
         }
     }
 }
